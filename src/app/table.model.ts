0110ce5a9d2da47cac708fbbfb68e87d384508dd
SOGUT <nathan.sogut@capgemini.com>
2020-11-22 00:39:03 +0100

    fourth commit

--- src/app/table.model.ts
+++ src/app/table.model.ts
@@ -5,11 +5,11 @@ export class TableModel {
     date:number
     plateform:string
     name:string
-    technologies:Array<string>
-    // technologies:Array<Technologies>
+    // technologies:Array<string>
+    technologies:Array<Technologies>
     clientProject:string
 
-    public constructor(projectCode:number, date:number, plateform:string, name:string, technologies:Array<string>, clientProject:string)
+    public constructor(projectCode:number, date:number, plateform:string, name:string, technologies:Array<Technologies>, clientProject:string)
     {
         this.projectCode=projectCode
         this.date=date
@@ -18,14 +18,4 @@ export class TableModel {
         this.technologies=technologies
         this.clientProject=clientProject
     }
-
-    // public constructor(tableObject :  TableObject)
-    // {
-    //     this.id=tableObject.id
-    //     this.date=tableObject.date
-    //     this.plateform=tableObject.plateform
-    //     this.name=tableObject.name
-    //     this.technology=tableObject.technology[].name
-    //     this.clientProject=tableObject.clientProject
-    // }
 }
